46f4d510542e5705cef230ce49882d7cd6817a72
fengb1 <fengb1@email.arizona.edu>
2020-12-16 12:38:51 -0700

    update

--- mynearbyplaces/src/ServerInterface/server.js
+++ mynearbyplaces/src/ServerInterface/server.js
@@ -1,8 +1,23 @@
-import places from "./data";
+let API = "https://nearbyplaces-api.herokuapp.com/";
 let server = {
-  getPlaces: () => {return places},
-  addPlaces:(value) => {places.push(value)},
-  addComment:(index, value) => {places[index].reviews.push(value)},
-  removePlace:(index) => {places.splice(index, 1)}
+  getPlaces: () => {return (fetch (API + "/places").then(x => x.json()));},
+  addPlaces:(name, city, state, description) => {
+    let place = {name: name, city: city, state: state, description: description};
+    return(fetch(API + '/place',
+    {method: "POST", headers: {"CONTENT-TYPE": "application/json"}, body: JSON.stringify(place)})
+    .then(x => x.json()));
+  },
+  addComment:(placeid, content, author) => {
+    let comment = {content: content, author: author};
+    return(fetch(API + `/review/${placeid}`,
+    {method: "POST", headers: {"CONTENT-TYPE": "application/json"}, body: JSON.stringify(comment)})
+    .then(x => x.json()));
+  },
+  removePlace:(name, city, state) => {
+    let place = {name: name, city: city, state: state};
+    return(fetch(API + '/remove',
+    {method: "POST", headers: {"CONTENT-TYPE": "application/json"}, body: JSON.stringify(place)})
+    .then(x => x.json()));
+  }
 };
 export default server;
